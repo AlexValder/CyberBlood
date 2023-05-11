0b69447fcc9ec9a20b2398a409fa3441ffce9d3d
Bohdan <bohdan.lysytsyn@gmail.com>
2023-05-11 21:22:51 +0300

    formatting

--- entities/props/ladder.gd
+++ entities/props/ladder.gd
@@ -16,8 +16,10 @@ var _bottom: float
 func get_center() -> float:
     return _node.global_position.x
 
+
 func get_ladder_width() -> float:
     return _shape.size.x
+
 
 func get_player_center() -> float:
     if facing_left:
