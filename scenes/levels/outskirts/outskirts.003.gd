774d70a881fd84198ecb1d1f3c6e8030c292743c
Alex Valder <angelaarhangelska@gmail.com>
2023-06-04 23:05:55 +0200

    NRE prevention

--- scenes/levels/outskirts/outskirts.003.gd
+++ scenes/levels/outskirts/outskirts.003.gd
@@ -10,26 +10,34 @@ extends BaseLevel
 func _ready() -> void:
     super._ready()
 
-    var shortcut = GameManager.save_data.get_map_change(biome, id, "shortcut")
-    if shortcut == "1":
-        _trapdoor.queue_free()
-        _hidden_area.queue_free()
-        _hiding.queue_free()
-
     var fight = GameManager.save_data.get_map_change(biome, id, "fight_over")
     if fight == "1":
         clear_arena()
 
 
 func clear_arena() -> void:
+    if _bottom_arena == null: return
+    if _breakable_floor == null: return
+
     _bottom_arena.queue_free()
+    _bottom_arena = null
     _breakable_floor.queue_free()
+    _breakable_floor = null
 
 
 func _on_lever_interacted() -> void:
-    _trapdoor.queue_free()
-    _hidden_area.queue_free()
-    _hiding.queue_free()
+    if _trapdoor != null:
+        _trapdoor.queue_free()
+        _trapdoor = null
+
+    if _hidden_area != null:
+        _hidden_area.queue_free()
+        _hidden_area = null
+
+    if _hiding != null:
+        _hiding.queue_free()
+        _hiding = null
+
     GameManager.save_data.push_map_change(biome, id, "shortcut", "1")
 
 
